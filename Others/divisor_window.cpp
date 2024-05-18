e1aff801a4018e954080b766688d93818ab165e4
Md Nihal <111081295+mdnihal5@users.noreply.github.com>
2024-05-18 12:05:08 +0530

    Create divisor_window.cpp

---
+++ Others/divisor_window.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int minimumOperationsToMakeKPeriodic(string word, int k) {
+        int n=word.size(),ans=n/k-1;
+        for(int i=k;2*i<=n;i++){
+            if(n%i or i%k) continue;
+            unordered_map<string,int>m;
+            for(int j=0;j<n;j+=k){
+                string sub=word.substr(j,k);
+                m[sub]++;
+            }
+            for(auto &i:m){
+                int x=n-i.first.size()*i.second;
+                ans=min<int>(ans,x/i.first.size());
+            }
+        }
+        return ans;
+    }
+};
