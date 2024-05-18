b2952b741f39200d7dd8cc741e55e1fb2938bfbc
Md Nihal <111081295+mdnihal5@users.noreply.github.com>
2024-05-18 12:11:47 +0530

    Update divisor_window.cpp

--- Others/divisor_window.cpp
+++ Others/divisor_window.cpp
@@ -3,7 +3,7 @@ public:
     int minimumOperationsToMakeKPeriodic(string word, int k) {
         int n=word.size(),ans=n/k-1;
         for(int i=k;2*i<=n;i++){
-            if(n%i or i%k) continue;
+            if(n%i or i%k) continue; // remove i%k
             unordered_map<string,int>m;
             for(int j=0;j<n;j+=k){
                 string sub=word.substr(j,k);
