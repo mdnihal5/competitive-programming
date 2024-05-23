bbf8b2e6907c336fd2a03ea066565918cef8f375
Md Nihal <111081295+mdnihal5@users.noreply.github.com>
2024-05-23 09:21:46 +0530

    Update SegmentTree.cpp

--- SegmentTree/SegmentTree.cpp
+++ SegmentTree/SegmentTree.cpp
@@ -56,3 +56,46 @@ public:
         return query(1, 0, n - 1, l, r);
     }
 };
+
+
+
+
+//
+
+class SegmentTree {
+    typedef int T;
+    static constexpr T unit = INT_MAX;        //change this {INT_MAX,INT_MIN,0}
+    T fun (T a, T b) { return min (a, b); } // (any associative fn)
+    vector<T> s; int n;
+  public:
+    SegmentTree (int n = 0, T def = unit) : s (2 * n, def), n (n) {}
+    void update (int pos, T val) {
+        for (s[pos += n] = val; pos /= 2;)
+            s[pos] = fun (s[pos * 2], s[pos * 2 + 1]);
+    }
+    T query (int b, int e) { // query [b, e)
+        T ra = unit, rb = unit;
+
+        for (b += n, e += n; b < e; b /= 2, e /= 2) {
+            if (b % 2) ra = fun (ra, s[b++]);
+
+            if (e % 2) rb = fun (s[--e], rb);
+        }
+
+        return fun (ra, rb);
+    }
+    T getfirst (int x, int l, int r) {
+        if (query (l, r + 1) > x) return -1;
+
+        int mid = -1;
+
+        while (l <= r) {
+            mid = (l + r) / 2;
+
+            if (query (l, mid + 1) <= x) r = mid - 1;
+            else l = mid + 1;
+        }
+
+        return l;
+    }
+};
