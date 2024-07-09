f8fb6232ba28c0929f62e5f7af65f229d3696b90
Md Nihal <111081295+mdnihal5@users.noreply.github.com>
2024-07-09 18:20:29 +0530

    Update string_hashing.cpp

--- Strings/string_hashing.cpp
+++ Strings/string_hashing.cpp
@@ -1,5 +1,7 @@
 https://www.codechef.com/problems/BREAKSTRING?tab=statement
 https://www.codechef.com/viewsolution/1067616677
+
+
 /***
  *
  * 64-bit hashing for vectors or strings
@@ -16,19 +18,19 @@ https://www.codechef.com/viewsolution/1067616677
 
 using namespace std;
 
-constexpr uint64_t mod = (1ULL << 61) - 1;
+constexpr uint64_t prime = (1ULL << 61) - 1;
 
 const uint64_t seed = chrono::system_clock::now().time_since_epoch().count();
-const uint64_t base = mt19937_64 (seed) () % (mod / 3) + (mod / 3);
+const uint64_t base = mt19937_64 (seed) () % (prime / 3) + (prime / 3);
 
 uint64_t base_pow[MAXLEN];
 
-int64_t modmul (uint64_t a, uint64_t b) {
+int64_t primemul (uint64_t a, uint64_t b) {
     uint64_t l1 = (uint32_t) a, h1 = a >> 32, l2 = (uint32_t) b, h2 = b >> 32;
     uint64_t l = l1 * l2, m = l1 * h2 + l2 * h1, h = h1 * h2;
-    uint64_t ret = (l & mod) + (l >> 61) + (h << 3) + (m >> 29) + (m << 35 >> 3) + 1;
-    ret = (ret & mod) + (ret >> 61);
-    ret = (ret & mod) + (ret >> 61);
+    uint64_t ret = (l & prime) + (l >> 61) + (h << 3) + (m >> 29) + (m << 35 >> 3) + 1;
+    ret = (ret & prime) + (ret >> 61);
+    ret = (ret & prime) + (ret >> 61);
     return ret - 1;
 }
 
@@ -36,7 +38,7 @@ void init() {
     base_pow[0] = 1;
 
     for (int i = 1; i < MAXLEN; i++) {
-        base_pow[i] = modmul (base_pow[i - 1], base);
+        base_pow[i] = primemul (base_pow[i - 1], base);
     }
 }
 
@@ -56,15 +58,15 @@ struct PolyHash {
         pref.resize (n + 3, 0), suff.resize (n + 3, 0);
 
         for (int i = 1; i <= n; i++) {
-            pref[i] = modmul (pref[i - 1], base) + ar[i - 1] + 997;
+            pref[i] = primemul (pref[i - 1], base) + ar[i - 1] + 997;
 
-            if (pref[i] >= mod) pref[i] -= mod;
+            if (pref[i] >= prime) pref[i] -= prime;
         }
 
         for (int i = n; i >= 1; i--) {
-            suff[i] = modmul (suff[i + 1], base) + ar[i - 1] + 997;
+            suff[i] = primemul (suff[i + 1], base) + ar[i - 1] + 997;
 
-            if (suff[i] >= mod) suff[i] -= mod;
+            if (suff[i] >= prime) suff[i] -= prime;
         }
     }
 
@@ -72,16 +74,16 @@ struct PolyHash {
         : PolyHash (vector<char> (str, str + strlen (str) ) ) {}
 
     uint64_t hash (int l, int r) {
-        int64_t h = pref[r + 1] - modmul (base_pow[r - l + 1], pref[l]);
-        return h < 0 ? h + mod : h;
+        int64_t h = pref[r + 1] - primemul (base_pow[r - l + 1], pref[l]);
+        return h < 0 ? h + prime : h;
     }
 
     uint64_t rhash (int l, int r) {
-        int64_t h = suff[l + 1] - modmul (base_pow[r - l + 1], suff[r + 2]);
-        return h < 0 ? h + mod : h;
+        int64_t h = suff[l + 1] - primemul (base_pow[r - l + 1], suff[r + 2]);
+        return h < 0 ? h + prime : h;
     }
     uint64_t combine (uint64_t hash1, uint64_t hash2, int len2) {
-        return (modmul (hash1, base_pow[len2]) + hash2) % mod;
+        return (primemul (hash1, base_pow[len2]) + hash2) % prime;
     }
 
 };
@@ -104,5 +106,4 @@ int main() {
 
     return 0;
 }
-
 
