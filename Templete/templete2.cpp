d8fc9f4183751f58e3a55eb06c565868e4e661d8
Md Nihal <111081295+mdnihal5@users.noreply.github.com>
2024-07-11 07:59:42 +0530

    Create templete2.cpp

---
+++ Templete/templete2.cpp
@@ -0,0 +1,32 @@
+#include "bits/stdc++.h"
+using namespace std;
+#define all(x) x.begin(),x.end()
+#define ALL(x) x.rbegin(),x.rend()
+#define int long long
+using i_64 = long long;
+using u_64 = uint64_t;
+using u_128 = __uint128_t;
+const int mod = 1e9 + 7, MAX = 1e7 + 7, N = 1e5 + 5, inf = 1e18 ;
+
+#ifndef ONLINE_JUDGE
+    #include "/home/anonymous/Templates/debug.hpp"
+#else
+    #define debug(x...);
+#endif
+
+/*=========================================== SOLUTION ===========================================*/
+
+void ____solve() {
+
+
+
+}
+
+/*============================================= MAIN =============================================*/
+
+int32_t main() {
+    int Test = 1;  cin >> Test;
+
+    for (int i = 0; i < Test; i++, ____solve(), cout << endl);
+}
+
