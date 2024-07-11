2a84e93862361fb7133d2bcb679496cf6ab4b711
Md Nihal <111081295+mdnihal5@users.noreply.github.com>
2024-07-11 07:59:12 +0530

    Update cp_templete.cpp

--- Templete/cp_templete.cpp
+++ Templete/cp_templete.cpp
@@ -13,20 +13,22 @@ using namespace std;
 typedef unsigned long long ull;
 #define endl "\n"
 #define int long long
-#define i_64 int64_t
 #define sz(a) (int)a.size()
 #define vi vector<int>
 #define pii pair<int,int>
 #define vpi vector<pii>
 #define all(x) x.begin(),x.end()
 #define ALL(x) x.rbegin(),x.rend()
-#define rep(x,n) for (int i = x; i <x+n; i++)
+#define rep(i,x,n) for (int i = x; i <x+n; i++)
+using i_64 = long long;
+using u_64 = uint64_t;
+using u_128 = __uint128_t;
 const int mod = 1e9 + 7, inf = LONG_LONG_MAX, N = 1e6 + 7, MAX = 1e14 + 4;
 
-template<class T, class V>istream& operator>>(istream &in, pair<T, V> &a) {in >> a.first >> a.second; return in;}
-template<class T>istream& operator>>(istream &in, vector<T> &a) {for (auto &i : a) {in >> i;} return in;}
-template<class T, class V>ostream& operator<<(ostream &os, pair<T, V> &a) {os << a.first << " " << a.second; return os;}
-template<class T>ostream& operator<<(ostream &os, vector<T> &a) {for (auto &i : a) {os << i << " ";} return os;}
+template<class T, class V>istream &operator>> (istream &in, pair<T, V> &a) {in >> a.first >> a.second; return in;}
+template<class T>istream &operator>> (istream &in, vector<T> &a) {for (auto &i : a) {in >> i;} return in;}
+template<class T, class V>ostream &operator<< (ostream &os, pair<T, V> &a) {os << a.first << " " << a.second; return os;}
+template<class T>ostream &operator<< (ostream &os, vector<T> &a) {for (auto &i : a) {os << i << " ";} return os;}
 
 /*----------------------------------------------- PBDS --------------------------------------------*/
 
@@ -41,28 +43,24 @@ using ordered_map = tree<key, value, cmp, rb_tree_tag, tree_order_statistics_nod
 /*----------------------------------------------- PBDS ends --------------------------------------------*/
 
 #ifndef ONLINE_JUDGE
-#include "/home/anonymous/Templates/debug.hpp"
+    #include "/home/anonymous/Templates/debug.hpp"
 #else
-#define debug(x...);
+    #define debug(x...);
 #endif
 
 /*=========================================== SOLUTION ===========================================*/
 
+void ____solve() {
 
 
-void __solve() {
-
-    
 
 }
 
-
-
 /*============================================= MAIN =============================================*/
 
-int32_t  main()
-{
+int32_t  main() {
     Md_Nihal;
-    int testcases = 1;    cin >> testcases;
-    for (int i = 0; i < testcases; i++,__solve(),cout << endl);
+    int Testcases = 1;    cin >> Testcases;
+
+    for (int i = 0; i < Testcases; i++, ____solve(), cout << endl);
 }
