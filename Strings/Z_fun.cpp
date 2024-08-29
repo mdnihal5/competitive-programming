44666a1f46531c1450b389b0ff7e1fb2782ea91f
Md Nihal <111081295+mdnihal5@users.noreply.github.com>
2024-08-29 09:47:28 +0530

    Create Z_fun.cpp

---
+++ Strings/Z_fun.cpp
@@ -0,0 +1,21 @@
+//zf[i] = max d, s. t. substr(0, d) == substr(i, d), zf[0] = 0
+template<typename I>
+vector<int> Z (I first, I last) {
+    const size_t n = last - first;
+    vector<int> zf (n);
+ 
+    for (int i = 1, l = 0, r = 0; i < n; ++i) {
+        zf[i] = max<int> (0, min (r - i, zf    [i - l]) );
+ 
+        while (i + zf[i] < n && * (first + zf[i]) == * (first + zf[i] + i) ) {
+            ++zf[i];
+        }
+ 
+        if (i + zf[i] > r) {
+            r = i + zf[i];
+            l = i;
+        }
+    }
+ 
+    return zf;
+}
