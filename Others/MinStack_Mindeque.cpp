f10a8d9fbf5a16712147d43aed458d3bc9f9a9f3
Md Nihal <111081295+mdnihal5@users.noreply.github.com>
2024-09-13 10:55:19 +0530

    Update MinStack_Mindeque.cpp

--- Others/MinStack_Mindeque.cpp
+++ Others/MinStack_Mindeque.cpp
@@ -1,51 +1,46 @@
-struct minstack {
-    stack<pair<int, int>> st;
-    int getmin() {return st.top().second;}
-    bool empty() {return st.empty();}
-    int size() {return st.size();}
+class MinStack {
+  public:
+    std::stack<std::pair<int, int>> st;
     void push (int x) {
-        int mn = x;
-
-        if (!empty() ) mn = min (mn, getmin() );
-
-        st.push ({x, mn});
+        int current_min = st.empty() ? x : std::min (x, st.top().second);
+        st.push ({x, current_min});
     }
-    void pop() {st.pop();}
-    int top() {return st.top().first;}
-    void swap (minstack &x) {st.swap (x.st);}
+    void pop() {if (!st.empty() ) st.pop(); }
+    int top() const { return st.top().first; }
+    int getMin() const { return st.top().second; }
+    bool empty() const { return st.empty(); }
+    int size() const { return st.size(); }
+    void swap (MinStack &other) {
+        st.swap (other.st);
+    }
 };
-
-struct mindeque {
-    minstack l, r, t;
+class MinDeque {
+  public:
+    MinStack l, r, t;
     void rebalance() {
-        bool f = false;
+        if (r.empty() ) l.swap (r);
 
-        if (r.empty() ) {f = true; l.swap (r);}
+        for (int sz = r.size() / 2; sz--; r.pop() ) t.push (r.top() );
 
-        int sz = r.size() / 2;
+        while (!r.empty() ) l.push (r.top() ), r.pop();
 
-        while (sz--) {t.push (r.top() ); r.pop();}
-
-        while (!r.empty() ) {l.push (r.top() ); r.pop();}
-
-        while (!t.empty() ) {r.push (t.top() ); t.pop();}
-
-        if (f) l.swap (r);
+        while (!t.empty() ) r.push (t.top() ), t.pop();
     }
-    int getmin() {
-        if (l.empty() ) return r.getmin();
+    int getMin() {
+        if (l.empty() ) return r.getMin();
 
-        if (r.empty() ) return l.getmin();
+        if (r.empty() ) return l.getMin();
 
-        return min (l.getmin(), r.getmin() );
+        return std::min (l.getMin(), r.getMin() );
     }
-    bool empty() {return l.empty() && r.empty();}
-    int size() {return l.size() + r.size();}
-    void push_front (int x) {l.push (x);}
-    void push_back (int x) {r.push (x);}
-    void pop_front() {if (l.empty() ) rebalance(); l.pop();}
-    void pop_back() {if (r.empty() ) rebalance(); r.pop();}
-    int front() {if (l.empty() ) rebalance(); return l.top();}
-    int back() {if (r.empty() ) rebalance(); return r.top();}
-    void swap (mindeque &x) {l.swap (x.l); r.swap (x.r);}
+    bool empty() { return l.empty() && r.empty(); }
+    int size() { return l.size() + r.size(); }
+    void push_front (int x) { l.push (x); }
+    void push_back (int x) { r.push (x); }
+    void pop_front() { if (l.empty() ) rebalance(); l.pop(); }
+    void pop_back() { if (r.empty() ) rebalance(); r.pop(); }
+    int front() { if (l.empty() ) rebalance(); return l.top(); }
+    int back() { if (r.empty() ) rebalance(); return r.top(); }
+    void swap (MinDeque &x) { l.swap (x.l), r.swap (x.r); }
 };
+
