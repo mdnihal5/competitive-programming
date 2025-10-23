55b98523ebe75119f9dd3d9c8f633e15dbfa629e
Md Nihal <111081295+mdnihal5@users.noreply.github.com>
2025-10-23 08:41:09 +0530

    Update LazySegmentTree.cpp

--- SegmentTree/LazySegmentTree.cpp
+++ SegmentTree/LazySegmentTree.cpp
@@ -1,108 +1,88 @@
-struct Node{
-    int mn, mx, ly;
-};
-struct segtree{
-    int n; 
-    vector<Node> st;
-    segtree(int N=0){
-        init(N);
-    }
-    void init(int N){
-        n = N;
-        st.assign(4*(n+5), {0,0,0});
-    }
-    void apply(int p, int val){
-        st[p].mn += val;
-        st[p].mx += val;
-        st[p].ly += val;
-    }
-    void push(int p){
-        if(st[p].ly != 0){
-            apply(p<<1, st[p].ly);
-            apply(p<<1|1, st[p].ly);
-            st[p].ly = 0;
-        }
-    }
-    void pull(int p){
-        st[p].mn = min(st[p<<1].mn, st[p<<1|1].mn);
-        st[p].mx = max(st[p<<1].mx, st[p<<1|1].mx);
-    }
-    void ra(int p, int l, int r, int L, int R, int val){
-        if(R<l or r<L) return ;
-        if(L<=l and r<=R){
-            apply(p, val);
-            return ;
-        }
-        push(p);
-        int m = (l+r)>>1;
-        ra(p<<1, l, m, L, R, val);
-        ra(p<<1|1, m+1, r, L, R, val);
-        pull(p);
-    }
-    void ra(int L, int R, int val){
-        if(L>R) return ;
-        ra(1, 0, n, L, R, val);
-    }
-    int pq(int p, int l, int r, int i){
-        if(l==r) return st[p].mn;
-        push(p);
-        int m = (l+r)>>1;
-        if(i<=m) return pq(p<<1, l, m, i);
-        else return pq(p<<1|1, m+1, r, i);
-    }
-    int pq(int p){
-        return pq(1, 0, n, p);
-    }
-    int fre(int p, int l, int r, int L, int R, int t){
-        if(R<l or r<L) return -1;
-        if(L<=l and r<=R){
-            if(t<st[p].mn or t>st[p].mx) return -1;
-            if(l==r) return l;
-        }
-        push(p);
-        int m = (l+r)>>1;
-        int res = fre(p<<1|1, m+1, r, L, R, t);
-        if(res!=-1) return res;
-        return fre(p<<1, l, m, L, R, t);
-    }
-    int fre(int L, int R, int t){
-        if(L>R) return -1;
-        return fre(1, 0, n, L, R, t);
-    }
-    
-};
-
 class Solution {
 public:
+    static const int N = 1e5 + 5;
+    static const int INF = 1e9;
+
+    int nxt[N];
+    struct Seg {
+        struct Node {
+            int l, r;
+            int mn, mx;
+            int lazy;
+        };
+        vector<Node> t;
+        Seg(int n): t(4*n+5) { build(1,1,n); }
+    
+        void build(int p,int l,int r){
+            t[p] = {l,r,0,0,0};
+            if(l==r) return;
+            int m=(l+r)>>1;
+            build(p<<1,l,m);
+            build(p<<1|1,m+1,r);
+        }
+        void apply(int p,int v){
+            t[p].mn+=v; t[p].mx+=v; t[p].lazy+=v;
+        }
+        void push(int p){
+            if(t[p].lazy){
+                apply(p<<1,t[p].lazy);
+                apply(p<<1|1,t[p].lazy);
+                t[p].lazy=0;
+            }
+        }
+        void pull(int p){
+            t[p].mn=min(t[p<<1].mn,t[p<<1|1].mn);
+            t[p].mx=max(t[p<<1].mx,t[p<<1|1].mx);
+        }
+        void range_add(int p,int L,int R,int v){
+            auto [l,r,_,__,___]=t[p];
+            if(R<l||r<L) return;
+            if(L<=l && r<=R){ apply(p,v); return; }
+            push(p);
+            range_add(p<<1,L,R,v);
+            range_add(p<<1|1,L,R,v);
+            pull(p);
+        }
+        int findrightval(int p,int val){
+            auto &nd=t[p];
+            if(nd.mn>val || nd.mx<val) return -1;
+            if(nd.l==nd.r) return (nd.mn==val? nd.l : -1);
+            push(p);
+            int res = findrightval(p<<1|1, val);
+            if(res!=-1) return res;
+            return findrightval(p<<1, val);
+        }
+    };
     int longestBalanced(vector<int>& nums) {
         int n = nums.size();
-        unordered_map<int, int> l;
-        vector<int> p(n+1, 0);
-        for(int i=1; i<=n; i++){
-            if(l.count(nums[i-1])) p[i] = l[nums[i-1]];
-            else p[i] = 0;
-            l[nums[i-1]] = i;
+        int cnt = 0;
+        unordered_map<int, int>vis;
+        Seg seg(n);
+        for (int i = n - 1; i >= 0; --i) {
+            int x = nums[i];
+            auto it = vis.find(x);
+            nxt[i+1] = (it==vis.end() ? 0 : it->second);
+            vis[x] = i+1;
         }
-        vector<vector<int>> b(n+1);
-        for(int i=1; i<=n; i++){
-            b[p[i]].push_back(i);
-        }
-        segtree st(n);
-        for(auto i:b[0]){
-            int sg = (nums[i-1]%2) ? -1 : 1;
-            st.ra(i, n, sg);
+        unordered_set<int> seen; seen.reserve(n*2);
+        for (int i = 1; i <= n; ++i) {
+            int x = nums[i-1];
+            if (seen.insert(x).second) {
+                int s = (x&1) ? +1 : -1;
+                seg.range_add(1, i, n, s);
+            }
         }
         int ans = 0;
-        for(int i=1; i<=n; i++){
-            int t = st.pq(i-1);
-            int r = st.fre(i, n, t);
-            if(r!=-1) ans = max(ans, r-i+1);
-            if(i<=n){
-                for(auto j:b[i]){
-                    int sg = (nums[j-1]%2) ? -1 : 1;
-                    st.ra(j, n, sg);
-                }
+        for (int L = 1; L <= n; ++L) {
+            int R0 = seg.findrightval(1, 0);
+            if (R0 != -1 && R0 >= L) {
+                ans = max(ans, R0 - L + 1);
             }
+            int x = nums[L-1];
+            int s = (x&1) ? +1 : -1;
+            int endR = (nxt[L]==0 ? n : nxt[L]-1);
+            if (L <= endR) seg.range_add(1, L, endR, -s);
+            seg.range_add(1, L, L, INF);
         }
         return ans;
     }
