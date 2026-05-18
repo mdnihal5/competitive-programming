cd273adc18f945d64c106c4d8252e872ffe56aa3
Md Nihal <111081295+mdnihal5@users.noreply.github.com>
2026-05-18 16:34:52 +0530

    Update rolling_hash_subarray.cpp

--- Maths/rolling_hash_subarray.cpp
+++ Maths/rolling_hash_subarray.cpp
@@ -1,24 +1,33 @@
-using ull = unsigned long long;
+class RollingHash {
+    using ll = long long;
 
-struct Hash {
+    ll mod1 = 1000000007, mod2 = 1000000009;
+    ll p1 = 91138233, p2 = 972663749;
 
-    ull P = chrono::high_resolution_clock::now().time_since_epoch().count() ^ (ull)new char;
-    vector<ull> h, p;
+    vector<ll> h1, h2, pw1, pw2;
 
-    Hash(const vector<int>& a) {
-
+public:
+    RollingHash(const vector<int>& a) {
         int n = a.size();
+        h1.assign(n + 1, 0);
+        h2.assign(n + 1, 0);
+        pw1.assign(n + 1, 1);
+        pw2.assign(n + 1, 1);
 
-        h.assign(n + 1, 0);
-        p.assign(n + 1, 1);
+        for (int i = 0; i < n; i++) {
+            pw1[i + 1] = pw1[i] * p1 % mod1;
+            pw2[i + 1] = pw2[i] * p2 % mod2;
 
-        for(int i = 0; i < n; i++)
-            p[i + 1] = p[i] * P,
-            h[i + 1] = h[i] * P + a[i] + 1;
+            h1[i + 1] = (h1[i] * p1 + a[i]) % mod1;
+            h2[i + 1] = (h2[i] * p2 + a[i]) % mod2;
+        }
     }
 
-    ull get(int l, int r) {
-
-        return h[r + 1] - h[l] * p[r - l + 1];
+    pair<ll,ll> get(int l, int r) {
+        ll x1 = (h1[r + 1] - h1[l] * pw1[r - l + 1]) % mod1;
+        ll x2 = (h2[r + 1] - h2[l] * pw2[r - l + 1]) % mod2;
+        if (x1 < 0) x1 += mod1;
+        if (x2 < 0) x2 += mod2;
+        return {x1, x2};
     }
 };
